ddd351886c5dd96a601062607bc18dde313c4886
Will Bickford <wbic16@gmail.com>
2012-07-21 11:59:07 -0500

    Bug fixes
    Also allowed 1-based indexing

--- thebook.js
+++ thebook.js
@@ -9,32 +9,34 @@ var writerId = 0;
 var chapters = new Array();
 function addChapter(id, title)
 {
-	chapters[id] = title;
+	chapters[id - 1] = title;
 }
 
 function addTOC()
 {
 	for (var i = 0; i < chapters.length; ++i)
 	{
-		chapterLink(i);
+		chapterLink(i + 1);
 	}
 }
 
 function chapterLink(id)
 {
-	var chapter = id + 1;
-	var title = chapters[id];
+	var chapter = id;
+	var title = chapters[id - 1];
 	if (title.length > 0)
 	{
 		document.write("<li><a href=\"#ch" + chapter + "\">" + title + "</a></li>\n");
 	}
 }
+
 function nextChapter()
 {
-	var chapter = writerId++;
+	var chapter = writerId + 1;
 	var title = chapters[writerId];
 	if (title.length > 0)
 	{
 		document.write("<a name=\"ch" + chapter + "\"><h1>" + title + "</h1></a>\n");
 	}
+	writerId++;
 }
